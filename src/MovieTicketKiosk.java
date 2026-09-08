0c2459f427dd1b60b763ef97396d956bb956c951
jasonzhuhinhui <2106416546@qq.com>
2026-09-08 12:32:08 +0800

    Finished part of  math

--- src/MovieTicketKiosk.java
+++ src/MovieTicketKiosk.java
@@ -14,10 +14,18 @@ public class MovieTicketKiosk {
         String bookingcode = (movie + "-" + code);
         return bookingcode;
     }
-
+    public static String getSeat(){
+        int row = (int)(Math.random()*10+1);
+        int seat = (int)(Math.random()*20+1);
+        String seatnum = "Row " + row + "    Seat " + seat;
+        return seatnum;
+    }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        double ticket = 12.50;
+        double popcorn = 6.75;
+        double drink = 4.25;
 
         System.out.println("=================================================================\n");
         System.out.println("Welcome to the Starlight Cinema kiosk!\n");
@@ -31,6 +39,30 @@ public class MovieTicketKiosk {
         int numPopcorn = input.nextInt();
         System.out.print("Please enter the number of drinks you like to purchase: ");
         int numDrink = input.nextInt();
+
+        double subtotal = ticket*numTicket + popcorn*numPopcorn + drink*numDrink;
+        int discount = (int)(Math.random()*10 +6);
+
+        System.out.println("=================================================================");
+        System.out.println("                 STARLIGHT CINEMA - MOVIE TICKET          ");
+        System.out.println("=================================================================");
+        System.out.println("Booking code   : " + getBookingCode(title));
+        System.out.println("Customer       : " + name + "  (" + getInitial(name)+")");
+        System.out.println("Movie          : " + title);
+        System.out.println("Seat           : " + getSeat());
+
+        System.out.println("=================================================================");
+        System.out.println("ITEM                       QTY       PRICE        TOTAL");
+        System.out.println("Ticket                     "+numTicket+"      "+"$"+ticket+"        $"+ticket*numTicket);
+        System.out.println("Popcorn                    "+numPopcorn+"       "+"$"+popcorn+"        $"+popcorn*numPopcorn);
+        System.out.println("Drink                      "+numDrink+"       "+"$"+drink+"        $"+drink*numDrink);
+
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("Subtotal                                                 $"+subtotal);
+        System.out.println("Member discount ("+discount+")                          -$"+discount*0.01*subtotal);
+        System.out.println("Tax (8.25%)");
+        
+
 
 
     }
